df37892721713f67f2b5153c4286cfda8f3feeaf
ish-u <anmolgupta520@gmail.com>
2021-11-20 11:23:07 +0530

    added Practical Exma

---
+++ Practical_Exam/Q2/index.js
@@ -0,0 +1,31 @@
+$(function () {
+  $(".container").css({
+    display: "flex",
+    "flex-direction": "column",
+    "justify-content": "center",
+    "align-items": "center",
+    width: "80vw",
+    margin: "0 auto",
+  });
+
+  var $veg = $(".veg");
+  var $nonVeg = $(".non-veg");
+  var $switch = $("#switch");
+  $nonVeg.hide();
+
+  $switch.click(() => {
+    var displayveg = $nonVeg.css("display");
+    if (displayveg === "none") {
+      $veg.hide();
+      $nonVeg.show();
+      $switch.text("Non-Veg");
+      $switch.addClass("non-veg-button");
+    } else {
+      $nonVeg.hide();
+      $veg.show();
+      $switch.text("Veg");
+      $switch.removeClass("non-veg-button");
+      $switch.addClass("veg-button");
+    }
+  });
+});
